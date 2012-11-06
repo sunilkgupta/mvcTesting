0beca5d20cd3c44a2c8111a1739e04fe919f5b89
sunil kumar <sunil.kumar@genisys-group.com>
2012-11-06 19:41:57 +0530

    saved

---
+++ mvcTesting/Models/ReserveBookTesting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace mvcTesting.Models
+{
+    public class ReserveBookTesting:DbContext
+    {
+        DbSet<InfoBook> InfoBook { get; set; }
+    }
+}
